d9e52ab1356f649df08e6ee1c6b277a6890f673c
br-ndt <nlax76@gmail.com>
2022-06-23 20:18:05 -0400

    modify registerClientHandler logging

--- src/services/socket/registerClientHandlers.js
+++ src/services/socket/registerClientHandlers.js
@@ -1,9 +1,10 @@
 const registerClientHandlers = (socket, socketTimeout) => {
   socket.on("connect", () => {
-    console.log(socket.current);
-  })
+    
+  });
 
-  socket.on("connect_error", () => {
+  socket.on("connect_error", (error) => {
+    console.log(`connect_error due to ${error.message}`);
     socketTimeout = setTimeout(() => socket.connect(), 5000);
   });
 }
